14282f81a7f04aa71c70c68b4d196bd1d40a702f
Artem Kozaev <artemkozaev@gmail.com>
2018-02-01 18:33:09 +0300

    Change uploads menu link rendering

--- app/overrides/spree/admin/layouts/admin.rb
+++ app/overrides/spree/admin/layouts/admin.rb
@@ -3,12 +3,7 @@ Deface::Override.new(
   name:          'uploads_main_menu_tabs',
   insert_bottom: '#main-sidebar',
   text:       <<-HTML
-                <% if current_spree_user.respond_to?(:has_spree_role?) && current_spree_user.has_spree_role?(:admin) %>
-                  <ul class="nav nav-sidebar">
-                    <%= tab 'Uploads', url: admin_uploads_path, icon: 'cloud-upload' %>
-                  </ul>
-                <% end %>
-                <% if current_spree_vendor %>
+                <% if can?(:display, Spree::Upload) %>
                   <ul class="nav nav-sidebar">
                     <%= tab 'Uploads', url: admin_uploads_path, icon: 'cloud-upload' %>
                   </ul>
