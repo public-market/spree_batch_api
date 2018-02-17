68ff9e4a4ef3fe23495ab6727fde617285d8a266
Artem Kozaev <artemkozaev@gmail.com>
2018-02-17 16:55:01 +0300

    Remove useless gems

---
+++ spec/support/spree.rb
@@ -0,0 +1,24 @@
+# Requires factories and other useful helpers defined in spree_core.
+require 'spree/testing_support/authorization_helpers'
+require 'spree/testing_support/controller_requests'
+require 'spree/testing_support/factories'
+require 'spree/testing_support/url_helpers'
+require 'spree/api/testing_support/helpers'
+
+RSpec.configure do |config|
+  config.include Spree::Api::TestingSupport::Helpers
+
+  # == URL Helpers
+  #
+  # Allows access to Spree's routes in specs:
+  #
+  # visit spree.admin_path
+  # current_path.should eql(spree.products_path)
+  config.include Spree::TestingSupport::UrlHelpers
+
+  # == Requests support
+  #
+  # Adds convenient methods to request Spree's controllers
+  # spree_get :index
+  config.include Spree::TestingSupport::ControllerRequests, type: :controller
+end
