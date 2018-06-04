cae94267f5eacf6f5423e1e3032543e35e613883
Nick Zhebrun <nikolay.zhebrun@gmail.com>
2018-06-04 17:54:42 +0300

    Catch retry errors and process item worker

--- app/workers/spree/import_inventory_item_worker.rb
+++ app/workers/spree/import_inventory_item_worker.rb
@@ -4,34 +4,42 @@ module Spree
   class ImportInventoryItemWorker
     include Sidekiq::Worker
     sidekiq_options queue: :upload, retry: 3, backtrace: true
-
-    # rubocop:disable Metrics/MethodLength
     def perform(item_json, options)
-      @upload = Upload.find_by(id: options['upload_id'])
+      upload = self.class.load_upload(options)
 
       begin
         inventory_provider.call(item_json.with_indifferent_access, options: options.with_indifferent_access)
       rescue ImportError => e
-        catch_error(ImportError.new(
-                      I18n.t('workers.spree.import_inventory_item_worker.invalid_item',
-                             index: options['index'],
-                             messages: e.message),
-                      e.object
-        ))
+        self.class.catch_error(upload, options, e.message)
       end
 
-      @upload.increment!(:processed) # rubocop:disable Rails/SkipsModelValidations
+      self.class.increment_processed(upload)
     end
-    # rubocop:enable Metrics/MethodLength
 
-    private
+    sidekiq_retries_exhausted do |msg, _ex|
+      Sidekiq.logger.error "Failed #{msg['class']} with #{msg['args']}: #{msg['error_message']}"
+      options = msg.dig('args', 1)
+      upload = load_upload(options)
+      increment_processed(upload)
+      catch_error(upload, options, msg['error_message'])
+    end
 
     def inventory_provider
       Spree::Inventory::Providers::DefaultVariantProvider
     end
 
-    def catch_error(error)
-      @upload.upload_errors.create(message: error.message)
+    def self.load_upload(options)
+      Upload.find_by(id: options['upload_id'])
+    end
+
+    def self.increment_processed(upload)
+      upload.increment!(:processed) # rubocop:disable Rails/SkipsModelValidations
+    end
+
+    def self.catch_error(upload, options, error_message)
+      upload.upload_errors.create(message: I18n.t('workers.spree.import_inventory_item_worker.invalid_item',
+                                                  index: options['index'],
+                                                  messages: error_message))
     end
   end
 end
