b316d5af2d318c0f42004ac078470455fa3bb767
Artem Kozaev <artemkozaev@gmail.com>
2018-07-16 14:11:00 +0300

    Add strong params to inventory controller

--- app/controllers/spree/api/v1/inventory_controller.rb
+++ app/controllers/spree/api/v1/inventory_controller.rb
@@ -2,14 +2,19 @@ module Spree
   module Api
     module V1
       class InventoryController < Spree::Api::BaseController
-        def update
-          authorize! :create, Product
+        def update # rubocop:disable Metrics/MethodLength
+          authorize!(:create, Product)
           file_path = save_content
+
+          options = {
+            provider: inventory_params[:provider],
+            product_type: inventory_params[:product_type]
+          }.merge(additional_inventory_params)
+
           @upload = Inventory::UploadFileAction.call(
             params[:content_format],
             file_path,
-            provider: params[:provider],
-            product_type: params[:product_type]
+            options
           )
         end
 
@@ -21,6 +26,14 @@ module Spree
           file.write(request.body.read)
           file.close
           file.path
+        end
+
+        def additional_inventory_params
+          {}
+        end
+
+        def inventory_params
+          params.permit(:product_type, :provider)
         end
       end
     end
